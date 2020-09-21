d43d6bd7698627fb4d17e0ac6ed5477c4e5d80b3
miguel <19872243+Miguel-ASM@users.noreply.github.com>
2020-09-21 18:34:21 +0200

    added dropdown to choose how many new points are added

--- appWidgets/pebblesWidget.py
+++ appWidgets/pebblesWidget.py
@@ -21,7 +21,16 @@ def returnPebblessWidget():
     # to display the value of pi and buttons widget to update everything
     plot_output = ipw.Output()
     html_output = ipw.HTML()
-    update_button = ipw.Button(description="Click Me!")
+    update_button = ipw.Button(description="Add points!")
+
+    # A dropdown widget to choose how many new points are added when
+    # update_button is clicked
+    dropdown_newpoints = ipw.Dropdown(
+        options=[1, 10, 100],
+        value=1,
+        description='New Points:',
+        disabled=False,
+    )
 
     # Add Initial plot to the plot_output
     with plot_output:
@@ -44,7 +53,7 @@ def returnPebblessWidget():
     @update_button.on_click
     def on_button_clicked(b):
         # add new point to the Montecarlo experiment
-        monte.addNewPoint()
+        monte.addNewPoint(dropdown_newpoints.value)
         # Update the plot
         with plot_output:
             clear_output(wait=True)
@@ -54,14 +63,23 @@ def returnPebblessWidget():
         # Update the widget printing the value of pi
         html_output.value = html_message_template.format(pi_value = str(monte) )
 
+    add_points_widget = ipw.VBox(
+        children = [
+            dropdown_newpoints,
+            update_button
+        ],
+        layout = ipw.Layout(align_items = 'flex-end')
+    )
+
     return ipw.HBox(
         children= [
             plot_output,
             ipw.VBox(
                 children = [
-                    update_button,
+                    add_points_widget,
                     html_output
                 ]
             )
-        ]
+        ],
+        layout = ipw.Layout(align_items = 'flex-end')
     )
