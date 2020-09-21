972e123a75e687e93b30db0ef64b7090c72f3323
miguel <19872243+Miguel-ASM@users.noreply.github.com>
2020-09-21 18:11:16 +0200

    Added div element to print value of pi with MonteCarlo method

--- appWidgets/pebblesWidget.py
+++ appWidgets/pebblesWidget.py
@@ -1,6 +1,8 @@
 import ipywidgets as ipw
 from IPython.display import display,clear_output
 
+from matplotlib.pyplot import close as close_fig
+
 # Make the parent folder visible to import my project packages
 from os.path import abspath,dirname
 import sys
@@ -15,27 +17,51 @@ def returnPebblessWidget():
     # Set an instance of the MontecarloApprox class
     monte = MontecarloApprox()
 
-    # Set an output widget to display the plot and a button widget to update it
+    # Set an output widget to display the plot, an HTML widget
+    # to display the value of pi and buttons widget to update everything
     plot_output = ipw.Output()
+    html_output = ipw.HTML()
     update_button = ipw.Button(description="Click Me!")
 
     # Add Initial plot to the plot_output
     with plot_output:
-        display( plotTheMonteCarloExperiment( [] ) )
+        fig = plotTheMonteCarloExperiment( [] )
+        display( fig )
+        close_fig( fig ) #close figs to liberate memory
+
+    # define initial value for the html_output
+    html_message_template ="""<div class="jumbotron">
+    <h3>
+    π ≈ {pi_value}
+    </h3>
+    </div>
+    """
+    html_output.value = html_message_template.format(pi_value = str(monte) )
 
     # Add a call back to the button that will update the Montecarlo Approx with a new point
     # and update the scatter plot.
     # Using a decorator
     @update_button.on_click
     def on_button_clicked(b):
+        # add new point to the Montecarlo experiment
         monte.addNewPoint()
+        # Update the plot
         with plot_output:
             clear_output(wait=True)
-            display( plotTheMonteCarloExperiment(monte.points) )
+            fig = plotTheMonteCarloExperiment(monte.points)
+            display( fig )
+            close_fig( fig ) #close figs to liberate memory
+        # Update the widget printing the value of pi
+        html_output.value = html_message_template.format(pi_value = str(monte) )
 
-    return ipw.VBox(
+    return ipw.HBox(
         children= [
-            update_button,
-            plot_output
+            plot_output,
+            ipw.VBox(
+                children = [
+                    update_button,
+                    html_output
+                ]
+            )
         ]
     )
